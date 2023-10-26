e7973eaabb8e2f5efe8c8891314adfbb94f550c4
Yasir <mdyasirkhan2003@gmail.com>
2023-10-27 02:08:12 +0530

    Uploaded MedHelp Project

---
+++ webapp/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.middleware.csrf import CsrfViewMiddleware
+from sklearn.preprocessing import StandardScaler
+from joblib import load
+import numpy as np
+import pandas as pd
+
+model = load('./ml_models/diabetes_model.joblib')
+dataset = pd.read_csv('./ml_models/diabetes.csv')
+X = dataset.drop(columns = 'Outcome', axis = 1)
+columns_to_drop = ['BP', 'ST', 'INS', 'DPF']
+X = X.drop(columns=columns_to_drop)
+scaler = StandardScaler()
+scaler.fit(X)
+
+model2 = load('./ml_models/breastcancer_model.joblib')
+dataset2 = pd.read_csv('./ml_models/breastcancer.csv')
+X2 = dataset2.drop(columns = 'Outcome', axis = 1)
+columns_to_drop = ['tm', 'sm', 'sym', 'fdm', 'rse', 'tse', 'pse', 'ase', 'sse', 'cse', 'cnse', 'cpse', 'symse', 'fdse', 'rw', 'tw', 'pw', 'aw', 'sw', 'cw', 'cnw', 'cpw', 'symw', 'fdw']
+X2 = X2.drop(columns=columns_to_drop)
+scaler2 = StandardScaler()
+scaler2.fit(X2)
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def bmi(request):
+    return render(request, 'bmi.html')
+
+def diabetes(request):
+    pg = request.POST.get('pgdb', '')  # Get the posted values or empty strings if not provided
+    gl = request.POST.get('gldb', '')
+    bm = request.POST.get('bmidb', '')
+    age = request.POST.get('agedb', '')
+
+    if request.method == 'POST':
+        data = (pg, gl, bm, age)
+        input_numpyarray = np.asarray(data)
+        input_reshaped = input_numpyarray.reshape(1, -1)
+        inp = scaler.transform(input_reshaped)
+        y_pred = model.predict(inp)
+        if y_pred[0] == 1:
+            result = "Diabetic"
+        else:
+            result = "Not Diabetic"
+    else:
+        result = None
+
+    # Pass the form data and result to the template
+    return render(request, 'diabetes.html', {'result': result, 'pg': pg, 'gl': gl, 'bm': bm, 'age': age})
+
+def breastcancer(request):
+    rm = request.POST.get('rm', '')  # Get the posted values or empty strings if not provided
+    pm = request.POST.get('pm', '')
+    am = request.POST.get('am', '')
+    cm = request.POST.get('cm', '')
+    cnm = request.POST.get('cnm', '')
+    cpm = request.POST.get('cpm', '')
+
+    if request.method == 'POST':
+        data = (rm, pm, am, cm, cnm, cpm)
+        input_numpyarray = np.asarray(data)
+        input_reshaped = input_numpyarray.reshape(1, -1)
+        inp = scaler2.transform(input_reshaped)
+        y_pred = model2.predict(inp)
+        if y_pred[0] == 1:
+            result = "Melignant"
+        else:
+            result = "Benign"
+    else:
+        result = None
+
+    # Pass the form data and result to the template
+    return render(request, 'breastcancer.html', {'result': result, 'rm': rm, 'pm': pm, 'am': am, 'cm': cm, 'cnm':cnm, 'cpm':cpm})
+
+def services(request):
+    return HttpResponse("This is Services Page")
+
+def contact(request):
+    return HttpResponse("This is Contact Page")
