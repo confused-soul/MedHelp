bd328c1a2ef995862d11165cf4f4a1f4fb8acc86
Yasir <mdyasirkhan2003@gmail.com>
2023-10-29 02:35:24 +0530

    updated

--- MEDHELP/settings.py
+++ MEDHELP/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "MEDHELP.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        'DIRS': [BASE_DIR / "template"],  #added manually
+        'DIRS': [BASE_DIR],  #added manually
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
